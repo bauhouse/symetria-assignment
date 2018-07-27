72055897cd4134e583b5305405036f7b006cbe6e
Stephen Bau <bauhouse@gmail.com>
2018-07-26 20:03:07 -0700

    Add data properties to Vue instance and render them to HTML

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        portfolioValue: 'C$19,500.31'
+        connected: false,
+        error: false,
+        errorMessage: '',
+        portfolio: [],
+        portfolioValue: 19500.31,
+        portfolioValueFormatted: 'C$19,500.31',
+        portfolioIncrease: true,
+        priceChange: 8700.86,
+        priceChangeFormatted: '+$8,700.86'
+
     }
 });
 
-console.log(GetWallets());
+const portfolio = GetWallets().then(response => {
+    console.log(response);
+    app.connected = true;
+    app.portfolio = response;
+}).catch(e => {
+    console.log(e);
+    app.connected = false;
+    app.error = true;
+    app.errorMessage = e;
+});
